afbbe3fe1fb95448c096a9add9fa173749e4b664
OAINTERNAL\santiago.restrepo-bu <santiago-restrepo.burgos@overactive.com>
2022-09-05 22:28:37 -0500

    Added GrayButton Ui Component

---
+++ src/components/GrayButton/GrayButton.styles.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const GrayButtonContainer = styled.button`
+  border: 1px solid black;
+`;
---
+++ src/components/GrayButton/index.js
@@ -0,0 +1,5 @@
+function GrayButton(props) {
+  return <button onClick={props.onClick}>{props.children}</button>;
+}
+
+export default GrayButton;
